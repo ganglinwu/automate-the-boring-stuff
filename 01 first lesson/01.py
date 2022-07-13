e43aa916766daeef913d6440363e12bf9365f4c2
ganglinwu <ganglin.wu@gmail.com>
2022-07-14 01:12:23 +0800

    added int function because input function always returns a string!

---
+++ 01 first lesson/01.py
@@ -0,0 +1,22 @@
+# This program says hello and asks for my name
+
+print('Hello World!')
+
+
+
+
+# ask for their name
+name = input('What is your name?')
+
+print('It is good to meet you, ' + name + '!')
+
+print(f"The length of your name is {len(name)}")
+
+
+
+
+
+#ask for their age
+age = input('What is your age?')
+
+print(f'You will be {int(age) + 1} in a year!') # note that input function returns a string!
